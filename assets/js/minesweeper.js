8de347e708825854d9dc0a4ea0e302c211ab299f
mtsferreirasilva <mtsferreirasilva@gmail.com>
2017-01-28 05:27:54 -0500

    Adding some styles

--- assets/js/minesweeper.js
+++ assets/js/minesweeper.js
@@ -1,3 +1,5 @@
 import MapBuilder from './map-builder';
+import Sidebar from './sidebar';
 
 let mapBuilder = new MapBuilder();
+let sidebar = new Sidebar();
---
+++ assets/js/sidebar.js
@@ -0,0 +1,11 @@
+export default class Sidebar {
+  constructor() {
+
+  }
+
+  // Timer
+
+  // Level
+
+  // Reset button
+}
