c30904b867e819b3d4fd568d73904fad3ddfd7b9
mtsferreirasilva <mtsferreirasilva@gmail.com>
2017-02-01 00:07:32 -0500

    Testing port 80

--- src/app.js
+++ src/app.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
   res.sendFile('index.html');
 })
 
-app.listen(8000, () => {
-  console.log('\x1b[36m\nGame running on http://localhost:8000\n\x1b[0m');
+app.listen(80, () => {
+  console.log('\x1b[36m\nGame running on http://localhost/\n\x1b[0m');
 })
