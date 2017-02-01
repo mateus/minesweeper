1afd05f3d8cbeaed3aed82ad397ba7c5d761094a
mtsferreirasilva <mtsferreirasilva@gmail.com>
2017-02-01 01:04:55 -0500

    Test with ES5

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-import express from 'express';
-
-let app = express();
+var express = require('express');
+var app = express();
 
 app.set('port', (process.env.PORT || 8000));
+app.set('view engine', 'html');
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/', function(request, res) {
   res.sendFile('index.html');
-})
+});
 
-app.listen(app.get('port'), () => {
-  console.log(`http://localhost/:${app.get('port')}`);
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
