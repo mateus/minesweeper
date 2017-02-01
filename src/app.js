3670db96e4740b4d54d8566f4cea79dea41f41e1
mtsferreirasilva <mtsferreirasilva@gmail.com>
2017-02-01 00:27:39 -0500

    Again test server

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.get('/', (req, res) => {
   res.sendFile('index.html');
 })
 
-app.listen(80, () => {
-  console.log('\x1b[36m\nGame running on http://localhost/\n\x1b[0m');
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`http://localhost/:${port}`);
 })
